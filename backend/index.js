a6ff4e6d8c98855695eff62f65344ce23950179a
Tojammel Hoque <tojammelhoque375@gmail.com>
2025-02-28 15:25:36 +0530

    Deploy

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,3 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import cookieParser from "cookie-parser";
-// import authRoutes from "./routes/auth.route.js";
-// import productRoutes from "./routes/product.route.js";
-// import cartRoutes from "./routes/cart.route.js";
-// import couponRoutes from "./routes/coupon.route.js";
-// import paymentRoutes from "./routes/payment.route.js";
-// import analyticsRoutes from "./routes/analytics.route.js";
-// import { connectDB } from "./lib/db.js";
-// dotenv.config();
-
-// const app = express();
-// const PORT = process.env.PORT || 5000;
-
-// // middleware
-// app.use(express.json({ limit: "50mb" }));
-// app.use(cookieParser());
-
-// app.use("/api/auth", authRoutes);
-// app.use("/api/products", productRoutes);
-// app.use("/api/cart", cartRoutes);
-// app.use("/api/coupons", couponRoutes);
-// app.use("/api/payments", paymentRoutes);
-// app.use("/api/analytics", analyticsRoutes);
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-//   connectDB();
-// });
-
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -39,11 +8,14 @@ import couponRoutes from "./routes/coupon.route.js";
 import paymentRoutes from "./routes/payment.route.js";
 import analyticsRoutes from "./routes/analytics.route.js";
 import { connectDB } from "./lib/db.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve()
 
 // Connect to Database Before Starting Server
 const startServer = async () => {
@@ -62,6 +34,13 @@ const startServer = async () => {
     app.use("/api/coupons", couponRoutes);
     app.use("/api/payments", paymentRoutes);
     app.use("/api/analytics", analyticsRoutes);
+
+
+    // Serve static files
+    app.use(express.static(path.join(__dirname, "frontend/dist")));
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
+    });
 
     // Global Error Handler
     app.use((err, req, res, next) => {
